72c499ad1ec0463fe20c87c5d2a54800cff72e60
Goku <choengchai@3dsinteractive.com>
2021-03-14 22:54:33 +0700

    Add product list & add product detail & add require material

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:singh_architecture/configs/config.dart';
 import 'package:singh_architecture/cores/context.dart';
+import 'package:singh_architecture/mocks/products/best_seller_products.dart';
+import 'package:singh_architecture/mocks/products/new_arrival_products.dart';
+import 'package:singh_architecture/mocks/products/product_detail.dart';
+import 'package:singh_architecture/mocks/products/products.dart';
+import 'package:singh_architecture/repositories/base_repository.dart';
 import 'package:singh_architecture/repositories/product_repository.dart';
+import 'package:singh_architecture/styles/colors.dart';
+import 'package:singh_architecture/styles/fonts.dart';
+import 'package:singh_architecture/utils/object_helper.dart';
+import 'package:singh_architecture/widgets/common/top_bar.dart';
+import 'package:singh_architecture/widgets/products/product_slider.dart';
 
 class ProductDetailPage extends StatefulWidget {
   final IContext context;
   final IConfig config;
+  final String id;
 
   ProductDetailPage({
     required this.context,
     required this.config,
+    required this.id,
   });
 
   @override
@@ -26,16 +39,237 @@ class ProductDetailPageState extends State<ProductDetailPage> {
   void initState() {
     super.initState();
 
-    productRepository =
-        widget.context.repositories().productRepository(widget.config);
+    this.productRepository = ProductRepository(
+      config: widget.config,
+      options: NewRepositoryOptions(
+        baseUrl: "${widget.config.baseAPI()}/products",
+        mockItems: [...mockProducts, ...mockNewArrivalProducts, ...mockBestSellerProducts],
+        mockItem: mockProductDetail,
+      ),
+    );
+
+    this.productRepository.get(widget.id, isMock: true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    this.productRepository.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Product Detail"),
-      ),
+    return StreamBuilder<bool>(
+      stream: this.productRepository.isLoadingSC.stream,
+      builder: (context, snapshot) {
+        if (ObjectHelper.isSnapshotStateLoading(snapshot)) {
+          return Center(
+            child: Container(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        return Container(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).padding.top,
+                color: colorPrimary,
+              ),
+              TopBar(
+                prefixWidget: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.only(
+                      left: 16,
+                    ),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                postfixWidget: Container(
+                  height: 85,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(
+                          right: 16,
+                        ),
+                        child: Icon(
+                          Icons.shopping_cart,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Positioned(
+                        top: 15,
+                        right: 5,
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                            color: colorSecondary,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Text(
+                            "1",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: [
+                      Container(
+                        child: ProductSlider(
+                          imageURLs:
+                              (this.productRepository.data?.Galleries ?? []),
+                        ),
+                      ),
+                      Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(
+                                bottom: 8,
+                              ),
+                              child: Text(
+                                this.productRepository.data?.Title ?? "",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: h3,
+                                  fontWeight: fontWeightBold,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      child: Text(
+                                        "฿ ${this.productRepository.data?.Price}",
+                                        style: TextStyle(
+                                          fontSize: h5,
+                                          fontWeight: fontWeightBold,
+                                          color: colorSecondary,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    child: Icon(
+                                      Icons.favorite_outline,
+                                      color: Colors.redAccent,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.only(
+                          top: 8,
+                          bottom: 8,
+                          left: 16,
+                          right: 16,
+                        ),
+                        child: Html(
+                            data:
+                                this.productRepository.data?.Description ?? ""),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          top: 16,
+                          bottom: 16,
+                        ),
+                        color: colorPrimary,
+                        alignment: Alignment.center,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(
+                                right: 8,
+                              ),
+                              child: Icon(
+                                Icons.shopping_cart,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Container(
+                              child: Text(
+                                "เพิ่มลงรถเข็น",
+                                style: TextStyle(
+                                  fontSize: h6,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          top: 16,
+                          bottom: 16,
+                        ),
+                        color: colorSecondary,
+                        alignment: Alignment.center,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(),
+                            Container(
+                              child: Text(
+                                "ซื้อสินค้า",
+                                style: TextStyle(
+                                  fontSize: h6,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -2,10 +2,12 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart';
 import 'package:singh_architecture/configs/config.dart';
+import 'package:singh_architecture/mocks/products/products.dart';
 import 'package:singh_architecture/models/product_model.dart';
 import 'package:singh_architecture/repositories/base_repository.dart';
 import 'package:singh_architecture/repositories/types.dart';
 import 'package:singh_architecture/utils/requester.dart';
+import 'package:singh_architecture/utils/time_helper.dart';
 
 class ProductRepository extends BaseDataRepository<ProductModel> {
   final IConfig config;
@@ -28,16 +30,48 @@ class ProductRepository extends BaseDataRepository<ProductModel> {
       }
 
       if (isMock) {
-        sleep(Duration(seconds: 2));
+        await TimeHelper.sleep();
         data = {"items": this.options.getMockItems()};
       } else {
-        Response response = await Requester.get(this.options.getBaseUrl(), params);
+        Response response =
+            await Requester.get(this.options.getBaseUrl(), params);
         Map<String, dynamic> js = json.decode(utf8.decode(response.bodyBytes));
         data = js;
       }
 
       this.items = ProductModel.toList(data["items"]);
       this.itemsSC.add(this.items);
+
+      this.toLoadedStatus();
+    } catch (e) {
+      this.toErrorStatus(e);
+    }
+  }
+
+  @override
+  Future get(String id,
+      {Map<String, dynamic>? params, bool isMock = false}) async {
+    try {
+      this.toLoadingStatus();
+      late Map<String, dynamic> data;
+
+      if (params == null) {
+        params = {};
+      }
+
+      if (isMock) {
+        await TimeHelper.sleep();
+        Map<String, dynamic> mock = this.options.getMockItems()!.firstWhere((element) => id == element["product_id"]);
+        data = {"data": mock};
+      } else {
+        Response response =
+            await Requester.get(this.options.getBaseUrl(), params);
+        Map<String, dynamic> js = json.decode(utf8.decode(response.bodyBytes));
+        data = js;
+      }
+
+      this.data = ProductModel.fromJson(data["data"]);
+      this.dataSC.add(this.data);
 
       this.toLoadedStatus();
     } catch (e) {
